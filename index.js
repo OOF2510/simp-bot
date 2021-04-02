9d3b70d9047dabb8d2d2adc3b798a454950acfe7
OOF2510 <gamekid01234@gmail.com>
2021-04-02 12:25:28 -0400

    allow deletion of info on guildAdd

--- index.js
+++ index.js
@@ -45,6 +45,43 @@ const getDefaultChannel = (guild) => {
    .first();
 }
 
+function findInMessage(message, target, {
+  caseSensitive = false,
+  author = false,
+  description = true,
+  footer = true,
+  title = true,
+  fields = true
+}) {
+  if (!target || !message) return null;
+  let str = caseSensitive ? target : target.toLowerCase();
+
+  if ((caseSensitive && message.content.includes(str)) ||
+    (!caseSensitive && message.content.toLowerCase().includes(str))) return true;
+
+  for (let embed of message.embeds) {
+    if ((caseSensitive && (
+        (author && embed.author.includes(str)) ||
+        (description && embed.description.includes(str)) ||
+        (footer && embed.footer.includes(str)) ||
+        (title && embed.title.includes(str)))) ||
+      (!caseSensitive && (
+        (author && embed.author.toLowerCase().includes(str)) ||
+        (description && embed.description.toLowerCase().includes(str)) ||
+        (footer && embed.footer.toLowerCase().includes(str)) ||
+        (title && embed.title.toLowerCase().includes(str))))
+    ) return true;
+
+    if (fields)
+      for (let field of embed.fields) {
+        if ((caseSensitive && [field.name, field.value].includes(str)) ||
+          (!caseSensitive && [field.name.toLowerCase(), field.value.toLowerCase()].includes(str))) return true;
+      }
+  }
+
+  return false;
+}
+
 const client = new Discord.Client();
 const preDB = new Keyv('sqlite://./prefixes.sqlite');
 const nbDB = new Keyv('sqlite://./nobroad.sqlite');
@@ -751,7 +788,7 @@ ${out}` + '```')
       await niDB.set(guild.id, 'true')
       addCh.messages.fetch()
         .then(messages => messages.filter(m => m.embeds.forEach((embed) => {
-          if (!embed.description == guild.id) return;
+          if (findInMessage(m, guild.id)) return;
           m.delete();
         })))
         .catch(console.error);
