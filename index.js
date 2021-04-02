5e0cd06dc554b3c74d6584433ad4ad8543b8a975
OOF2510 <gamekid01234@gmail.com>
2021-04-02 18:14:00 -0400

    add purge command

--- index.js
+++ index.js
@@ -773,9 +773,10 @@ ${out}` + '```')
       if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.channel.send('You dont have permissions to do that!')
       if (!args[0]) return msg.channel.send(`Usage: ${prefix}purge <number of messages to purge>`)
       var Num = args[0]
-      var num = Number(Num)
+      var nUm = Number(Num)
+      var num = nUm + 1
       if (isNaN(num)) return msg.channel.send('Please provide a valid number!')
-      if (num > 1000) return msg.channel.send('Cannot delete more than 1000 messages at a time!')
+      if (num > 1000) return msg.channel.send('Cannot delete more than 999 messages at a time!')
       channel.bulkDelete(num)
         .then(msgs => channel.send(`Deleted ${msgs.size} messages!`))
     } else return;
