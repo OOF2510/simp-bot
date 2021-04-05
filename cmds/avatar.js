e05e93637ba59fddd3a03f152879b96a8a8a1175
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-04-05 18:58:33 -0400

    a bit smaller avatars

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -32,7 +32,7 @@ module.exports = {
 
     let userNick = mem ? mem.displayName : user.username;
 
-    let av = user.avatarURL({ dynamic: true, size: 2048 });
+    let av = user.avatarURL({ dynamic: true, size: 1024 });
 
     let avEm = new Discord.MessageEmbed()
       .setTitle(`${userNick}'s Avatar`)
