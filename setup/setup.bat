ae577b0da28b4aa4aa4a3f44c5b97676c8bd9605
Nate T <64423517+OOF2510@users.noreply.github.com>
2021-04-17 22:33:49 -0400

    Create setup.bat

---
+++ setup/setup.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set /P prefix="Prefix: "
+set /P token="Bot Token: "
+set /P clientID="Bot Client ID: "
+set /P clientSecret="Client Secret: "
+set /P sessionSecret="Random String (session secret): "
+set /P email="Bot Email: "
+set /P emailPass="Bot Email Password: "
+type nul > config.json
+(
+echo {
+echo "prefix": "%prefix%",
+echo "token": "%token%",
+echo "clientID": "%clientID%",
+echo "clientSecret": "%clientSecret%",
+echo "sessionSecret": "%sessionSecret%",
+echo "email": "%email%",
+echo "emailPass": "%emailPass%"
+echo }
+) > config.json
+echo config.json:
+type config.json
+yarn install
+exit
