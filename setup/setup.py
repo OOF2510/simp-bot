aadc4ebdb55fc6dcda3a7884aea0495e45f54815
Nate T <64423517+OOF2510@users.noreply.github.com>
2021-04-17 23:28:50 -0400

    Create setup.py

---
+++ setup/setup.py
@@ -0,0 +1,30 @@
+import os
+
+prefix = input("Prefix: ")
+token = input("Token: ")
+clientID = input("Bot Client ID: ")
+clientSecret = input("Client Secret: ")
+sessionSecret = input("Random String (session secret): ")
+email = input("Bot Email: ")
+emailPass = input("Bot Email Password: ")
+
+configJson = "{" + f"""
+  \"prefix\": \"{prefix}\",
+  \"token\": \"{token}\",
+  \"clientID\": \"{clientID}\",
+  \"clientSecret\": \"{clientSecret}\",
+  \"sessionSecret\": \"{sessionSecret}\",
+  \"email\": \"{email}\",
+  \"emailPass\": \"{emailPass}\" 
+""" + "}"
+
+config = open("config.json", "w")
+config.write(configJson)
+config.close()
+
+config = open("config.json", "r")
+print("config.json:")
+print(config.read())
+config.close()
+
+os.system("yarn install")
