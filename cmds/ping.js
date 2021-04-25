199130c20838149b25bf2e495e930a1e24c7e07d
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-04-25 16:27:43 -0400

    format code

--- cmds/ping.js
+++ cmds/ping.js
@@ -26,17 +26,23 @@ module.exports = {
     bchDB,
     blDB
   ) {
-    var MongoPing0 = await exec(`ping -c 1 ${config.mongoShards[0]} | grep time=`)
-    var MongoPing1 = await exec(`ping -c 1 ${config.mongoShards[1]} | grep time=`)
-    var MongoPing2 = await exec(`ping -c 1 ${config.mongoShards[2]} | grep time=`)
+    var MongoPing0 = await exec(
+      `ping -c 1 ${config.mongoShards[0]} | grep time=`
+    );
+    var MongoPing1 = await exec(
+      `ping -c 1 ${config.mongoShards[1]} | grep time=`
+    );
+    var MongoPing2 = await exec(
+      `ping -c 1 ${config.mongoShards[2]} | grep time=`
+    );
 
-    var mongoping0 = MongoPing0.stdout.trim()
-    var mongoping1 = MongoPing1.stdout.trim()
-    var mongoping2 = MongoPing2.stdout.trim()
+    var mongoping0 = MongoPing0.stdout.trim();
+    var mongoping1 = MongoPing1.stdout.trim();
+    var mongoping2 = MongoPing2.stdout.trim();
 
-    var mongoPing0 = Number(mongoping0.split("time=").pop().replace(" ms", ``))
-    var mongoPing1 = Number(mongoping1.split("time=").pop().replace(" ms", ``))
-    var mongoPing2 = Number(mongoping2.split("time=").pop().replace(" ms", ``))
+    var mongoPing0 = Number(mongoping0.split("time=").pop().replace(" ms", ``));
+    var mongoPing1 = Number(mongoping1.split("time=").pop().replace(" ms", ``));
+    var mongoPing2 = Number(mongoping2.split("time=").pop().replace(" ms", ``));
 
     var MongoPing = mongoPing0 + mongoPing1 + mongoPing2;
     var mongoPing = Math.round(MongoPing / 3);
