b1ea446b010f148f799e5a27176a1c7ebc7d8be7
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-04-25 16:09:57 -0400

    add database ping

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,32 +1,7 @@
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-// function ping(host, port, pong) {
-//   var started = new Date().getTime();
-
-//   var http = new XMLHttpRequest();
-
-//   http.open("GET", "http://" + host + ":" + port, /*async*/ true);
-//   http.onreadystatechange = function () {
-//     if (http.readyState == 4) {
-//       var ended = new Date().getTime();
-
-//       var milliseconds = ended - started;
-
-//       if (pong != null) {
-//         pong(milliseconds);
-//       }
-//     }
-//   };
-//   try {
-//     http.send(null);
-//   } catch (exception) {
-//     // this is expected
-//   }
-// }
-
 module.exports = {
   name: "ping",
   aliases: ["p"],
-  execute(
+  async execute(
     msg,
     args,
     client,
@@ -51,38 +26,27 @@ module.exports = {
     bchDB,
     blDB
   ) {
-    // var mongoPings = [];
+    var MongoPing0 = await exec(`ping -c 1 ${config.mongoShards[0]} | grep time=`)
+    var MongoPing1 = await exec(`ping -c 1 ${config.mongoShards[1]} | grep time=`)
+    var MongoPing2 = await exec(`ping -c 1 ${config.mongoShards[2]} | grep time=`)
 
-    // var mongoPing0;
-    // ping(config.mongoShards.zero, 27017, function (p) {
-    //   mongoPing0 = Number(p);
-    //   mongoPings.push(mongoPing0);
-    // });
-    // var mongoPing1;
-    // ping(config.mongoShards.one, 27017, function (p) {
-    //   mongoPing0 = Number(p);
-    //   mongoPings.push(mongoPing1);
-    // });
-    // var mongoPing2;
-    // ping(config.mongoShards.two, 27017, function (p) {
-    //   mongoPing2 = Number(p);
-    //   mongoPings.push(mongoPing2);
-    // });
+    var mongoping0 = MongoPing0.stdout.trim()
+    var mongoping1 = MongoPing1.stdout.trim()
+    var mongoping2 = MongoPing2.stdout.trim()
 
-    // console.log(mongoPings);
-    // mongoPing0 = mongoPings[0];
-    // mongoPing1 = mongoPings[1];
-    // mongoPing2 = mongoPings[2];
+    var mongoPing0 = Number(mongoping0.split("time=").pop().replace(" ms", ``))
+    var mongoPing1 = Number(mongoping1.split("time=").pop().replace(" ms", ``))
+    var mongoPing2 = Number(mongoping2.split("time=").pop().replace(" ms", ``))
 
-    // var MongoPing = mongoPing0 + mongoPing1 + mongoPing2;
-    // var mongoPing = MongoPing / 3;
+    var MongoPing = mongoPing0 + mongoPing1 + mongoPing2;
+    var mongoPing = Math.round(MongoPing / 3);
 
     const pingEm = new Discord.MessageEmbed()
       .setTitle(`Pong UwU!`)
       .setAuthor(botNick, client.user.avatarURL())
       .addFields(
         { name: `Bot Ping`, value: `\`${client.ws.ping}ms\``, inline: true },
-        // { name: `Database Ping`, value: `\`${mongoPing}ms\``, inline: true }
+        { name: `Database Ping`, value: `\`${mongoPing}ms\``, inline: true }
       )
       .setColor("RANDOM");
 
