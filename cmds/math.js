71e544309cadc2b5ac98c512554a3fabdbf2f270
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-04-26 17:22:22 -0400

    fix math command

--- cmds/math.js
+++ cmds/math.js
@@ -33,10 +33,10 @@ module.exports = {
       let operation = args[1];
       let num2 = Number(args[2]);
 
-      if (!num1) return message.channel.send("Num1 needs to be specified!");
+      if (!num1) return channel.send("Num1 needs to be specified!");
       if (!operation)
-        return message.channel.send("An operation was not specified!");
-      if (!num2) return message.channel.send("Num2 needs to be specified!");
+        return channel.send("An operation was not specified!");
+      if (!num2) return channel.send("Num2 needs to be specified!");
 
       channel.send(`Answer: ${math.calculate(num1, operation, num2)}`);
     } catch (e) {
