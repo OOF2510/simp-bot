86db0c2bbb376b0a66417c5020704a5ba417bfe7
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-04-28 17:17:56 -0400

    add git command

---
+++ cmds/github.js
@@ -0,0 +1,37 @@
+module.exports = {
+  name: "github",
+  aliases: ["git", "source", "code", "gh", "src"],
+  async execute(
+    msg,
+    args,
+    client,
+    channel,
+    author,
+    server,
+    guild,
+    botMem,
+    botNick,
+    testServer,
+    defChannel,
+    me,
+    allowed,
+    prefix,
+    config,
+    exec,
+    os,
+    Discord,
+    preDB,
+    nbDB,
+    niDB,
+    bchDB,
+    blDB
+  ) {
+    const gitEm = new Discord.MessageEmbed()
+      .setTitle("View the source code on GitHub!")
+      .setURL("https://github.com/OOF2510/simp-bot-rewritten")
+      .setColor("RANDOM")
+      .setTimestamp("RANDOM");
+
+    channel.send(gitEm);
+  },
+};
