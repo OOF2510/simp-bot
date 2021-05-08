4c36de3ca61741af56bfa78dd91d4dad0b500ff3
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-05-08 15:59:01 -0400

    remove dashboard for now

--- dash/modules/auth-client.js
+++
@@ -1,8 +0,0 @@
-const OAuthClient = require("disco-oauth");
-const config = require("../../config.json");
-
-const client = new OAuthClient(config.clientID, config.clientSecret);
-client.setRedirect(config.dashboardURL);
-client.setScopes("identify", "guilds");
-
-module.exports = client;
