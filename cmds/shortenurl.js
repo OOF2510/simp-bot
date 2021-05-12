a0e22fddd8758661f7933290ea5ae78b8e788b99
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-05-12 17:33:48 -0400

    make shortenurl command

---
+++ cmds/shortenurl.js
@@ -0,0 +1,52 @@
+var shortUrl = require("node-url-shortener");
+
+function isURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
+module.exports = {
+  name: "shortenurl",
+  aliases: ["urlshorten", "shorturl"],
+  async execute(
+    msg,
+    args,
+    client,
+    channel,
+    author,
+    server,
+    guild,
+    botMem,
+    botNick,
+    testServer,
+    defChannel,
+    me,
+    allowed,
+    prefix,
+    config,
+    exec,
+    os,
+    Discord,
+    preDB,
+    nbDB,
+    bchDB,
+    blDB
+  ) {
+    if (!args[0]) return channel.send("You must provide a url");
+    if (!isURL(args[0])) return channel.send("That is not a valid URL!");
+
+    let url = args[0];
+
+    shortUrl.short(url, function (err, url) {
+      msg.reply(`<${url}>`);
+    });
+  },
+};
