a8c800538274e8e5911ec1a0b0f0a2c066c0b0a0
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-05-25 18:11:17 -0400

    add restart command back

---
+++ cmds/restart.js
@@ -0,0 +1,43 @@
+module.exports = {
+  name: "restart",
+  execute(
+    msg,
+    args,
+    client,
+    channel,
+    author,
+    server,
+    guild,
+    botMem,
+    botNick,
+    testServer,
+    defChannel,
+    me,
+    allowed,
+    prefix,
+    config,
+    exec,
+    os,
+    Discord,
+    preDB,
+    nbDB,
+    niDB,
+    bchDB,
+    blDB
+  ) {
+    if (allowed.includes(author.id)) {
+      msg.reply(`Restarting UwU!`);
+      client.destroy();
+      client.login(config.token);
+      client.user.setActivity(`${client.guilds.cache.size} servers! | s!help`, {
+        type: "WATCHING",
+      });
+      console.log(`Restarted because ${msg.author.username} told me to!`);
+      setTimeout(() => {
+        msg.channel.send(`Successfully restarted!`);
+      }, 2000);
+    } else {
+      msg.reply(`Only the developer & certian whitelisted users can do that!`);
+    }
+  },
+};
