7db0dafef3a38d8edf649cc1273c7510f2907617
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-06-22 10:49:46 -0400

    Revert

--- cmds/info.js
+++ cmds/info.js
@@ -1,4 +1,47 @@
-const osinfo = require("@oof2510/osinfo")
+const si = require('systeminformation');
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+function millisecondsToStr(milliseconds) {
+  // TIP: to find current time in milliseconds, use:
+  // var  current_time_milliseconds = new Date().getTime();
+
+  function numberEnding(number) {
+    return number > 1 ? "s" : "";
+  }
+
+  var temp = Math.floor(milliseconds / 1000);
+  var years = Math.floor(temp / 31536000);
+  if (years) {
+    return years + " year" + numberEnding(years);
+  }
+  //TODO: Months! Maybe weeks?
+  var days = Math.floor((temp %= 31536000) / 86400);
+  if (days) {
+    return days + " day" + numberEnding(days);
+  }
+  var hours = Math.floor((temp %= 86400) / 3600);
+  if (hours) {
+    return hours + " hour" + numberEnding(hours);
+  }
+  var minutes = Math.floor((temp %= 3600) / 60);
+  if (minutes) {
+    return minutes + " minute" + numberEnding(minutes);
+  }
+  var seconds = temp % 60;
+  if (seconds) {
+    return seconds + " second" + numberEnding(seconds);
+  }
+  return "less than a second"; //'just now' //or other string you like;
+}
 
 module.exports = {
   name: "info",
@@ -30,6 +73,35 @@ module.exports = {
     bchDB,
     blDB
   ) {
+    function isLinux() {
+      let osPlat = os.platform();
+      if (osPlat == "linux") return true;
+      else return false;
+    }
+
+    var uptimeMilsec = os.uptime() * 1000;
+    var uptime = millisecondsToStr(uptimeMilsec);
+    var botUptimeMilsec = process.uptime() * 1000;
+    var botUptime = millisecondsToStr(botUptimeMilsec);
+    var memUsageB = process.memoryUsage().heapUsed;
+    var memUsage = formatBytes(memUsageB);
+    var totalSysMem = os.totalmem();
+    var freeSysMem = os.freemem();
+    var usedSysMem = totalSysMem - freeSysMem;
+    var sysMemUsage = formatBytes(usedSysMem);
+    var CpuTemp = await si.cpuTemperature();
+    var cpuTemp = CpuTemp.main
+
+    let Distro;
+    let distro;
+    if (isLinux())
+      Distro = await exec('hostnamectl | grep -i "operating system"');
+    if (!Distro) distro = "Error getting distro";
+    else distro = Distro.stdout.trim().replace("Operating System: ", ``);
+
+    let NodeV = await exec("node -v");
+    let nodeV = NodeV.stdout.trim();
+
     let YayFetch = await exec("yayfetch --hide-logo");
     let yayFetch = YayFetch.stdout.trim();
 
@@ -59,22 +131,22 @@ module.exports = {
           value: `Information about the bot's OS`,
           inline: false,
         },
-        { name: "Platform", value: "`" + osinfo.os.platform + "`", inline: true },
-        { name: "Arch", value: "`" + osinfo.os.arch + "`", inline: true },
-        { name: "Release", value: "`" + osinfo.os.release + "`", inline: true },
-        { name: "Version", value: "`" + osinfo.os.version + "`", inline: true },
-        { name: "Linux Distro", value: "`" + osinfo.os.linuxDistro + "`", inline: true },
-        { name: "Bot RAM Usage", value: "`" + osinfo.process.memUsage + "`", inline: true },
+        { name: "Platform", value: "`" + os.platform() + "`", inline: true },
+        { name: "Arch", value: "`" + os.arch() + "`", inline: true },
+        { name: "Release", value: "`" + os.release() + "`", inline: true },
+        { name: "Version", value: "`" + os.version() + "`", inline: true },
+        { name: "Linux Distro", value: "`" + distro + "`", inline: true },
+        { name: "Bot RAM Usage", value: "`" + memUsage + "`", inline: true },
         {
           name: "System RAM Usage",
-          value: "`" + osinfo.system.memUsage + "`",
+          value: "`" + sysMemUsage + "`",
           inline: true,
         },
-        { name: `NodeJS Version`, value: "`" + osinfo.nodejsVersion + "`", inline: true },
+        { name: `NodeJS Version`, value: "`" + nodeV + "`", inline: true },
         { name: `Discord.js Version`, value: "`" + djsV + "`", inline: true },
-        { name: `System Uptime`, value: "`" + osinfo.system.uptime + "`", inline: true },
-        { name: `Bot Uptime`, value: "`" + osinfo.process.uptime + "`", inline: true },
-        { name: `CPU Temperature`, value: "`" + osinfo.system.cpuTemp + "℃`", inline: true }
+        { name: `System Uptime`, value: "`" + uptime + "`", inline: true },
+        { name: `Bot Uptime`, value: "`" + botUptime + "`", inline: true },
+        { name: `CPU Temperature`, value: "`" + cpuTemp + "℃`", inline: true }
       )
       .setColor(config.embedColor);
 
