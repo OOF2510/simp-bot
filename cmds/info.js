bd44471d8ffc9367acc9385b6018ef0e4ec678fe
OOF2510 <OOF2510@users.noreply.github.com>
2021-08-17 01:43:49 +0000

    [auto] Code formatting

--- cmds/info.js
+++ cmds/info.js
@@ -11,43 +11,43 @@ function formatBytes(bytes, decimals = 2) {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 function millisecondsToStr(milliseconds) {
-var seconds = {},
+  var seconds = {},
     minutes = {},
-    hours   = {},
-    days    = {},
-    months  = {},
-    years   = {};
+    hours = {},
+    days = {},
+    months = {},
+    years = {};
 
-seconds.val = Math.floor(milliseconds / 1000)
-minutes.val = Math.floor(seconds.val / 60)
-hours.val   = Math.floor(minutes.val / 60)
-days.val    = Math.floor(hours.val / 24)
-months.val  = Math.floor(days.val / 30)
-years.val   = Math.floor(days.val / 365);
+  seconds.val = Math.floor(milliseconds / 1000);
+  minutes.val = Math.floor(seconds.val / 60);
+  hours.val = Math.floor(minutes.val / 60);
+  days.val = Math.floor(hours.val / 24);
+  months.val = Math.floor(days.val / 30);
+  years.val = Math.floor(days.val / 365);
 
-seconds.val %= 60;
-minutes.val %= 60;
-hours.val %= 24;
-days.val %= 30;
-months.val %= 12;
+  seconds.val %= 60;
+  minutes.val %= 60;
+  hours.val %= 24;
+  days.val %= 30;
+  months.val %= 12;
 
-seconds.name = "sec"
-minutes.name = "min"
-hours.name = "hr"
-days.name = "day"
-months.name = "month"
-years.name = "yr"
+  seconds.name = "sec";
+  minutes.name = "min";
+  hours.name = "hr";
+  days.name = "day";
+  months.name = "month";
+  years.name = "yr";
 
-var units = [years, months, days, hours, minutes, seconds]
-var toReturn = []
+  var units = [years, months, days, hours, minutes, seconds];
+  var toReturn = [];
 
-for (const unit of units) {
-  if (unit.val === 1) toReturn.push(`${unit.val} ${unit.name}`)
-  else if (unit.val > 1) toReturn.push(`${unit.val} ${unit.name}s`)
-}
+  for (const unit of units) {
+    if (unit.val === 1) toReturn.push(`${unit.val} ${unit.name}`);
+    else if (unit.val > 1) toReturn.push(`${unit.val} ${unit.name}s`);
+  }
 
-let str = toReturn.join(", ")
-return str
+  let str = toReturn.join(", ");
+  return str;
 }
 
 module.exports = {
