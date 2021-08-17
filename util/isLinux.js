830acf7f5a9da2b64f77256015a73e06f48354d0
OOF2510 <OOF2510@users.noreply.github.com>
2021-08-17 19:10:27 +0000

    [auto] Code formatting

--- util/isLinux.js
+++ util/isLinux.js
@@ -1,7 +1,7 @@
-let os = require('os')
+let os = require("os");
 
 module.exports = function () {
-    let osPlat = os.platform();
-    if (osPlat == "linux") return true;
-    else return false;
-}
+  let osPlat = os.platform();
+  if (osPlat == "linux") return true;
+  else return false;
+};
