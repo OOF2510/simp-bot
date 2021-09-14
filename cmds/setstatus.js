641a10f834b1c5aafa5429cdad320d09249f1d7e
OOF2510 <OOF2510@users.noreply.github.com>
2021-09-14 00:17:12 +0000

    [auto] Code formatting

--- cmds/setstatus.js
+++ cmds/setstatus.js
@@ -23,21 +23,30 @@ module.exports = {
     exec,
     os,
     Discord,
-    db) {
-	if (!allowed.includes(msg.author.id)) return channel.send(`Only the developer & certian whitelisted users can use that command!`);
-	if (!args[0]) return msg.reply(`usage: ${this.usage}`)
-	if (args[0].toLowerCase() === 'reset') {
-	await client.user.setActivity(
-    `${client.guilds.cache.size} servers! | ${config.prefix}help`,
-    {
-      type: "WATCHING",
+    db
+  ) {
+    if (!allowed.includes(msg.author.id))
+      return channel.send(
+        `Only the developer & certian whitelisted users can use that command!`
+      );
+    if (!args[0]) return msg.reply(`usage: ${this.usage}`);
+    if (args[0].toLowerCase() === "reset") {
+      await client.user.setActivity(
+        `${client.guilds.cache.size} servers! | ${config.prefix}help`,
+        {
+          type: "WATCHING",
+        }
+      );
+      return msg.reply("done");
     }
-  );
-return msg.reply('done')
-}
-	if (!["PLAYING","STREAMING","LISTENING", "WATCHING", "COMPETING"].includes(args[0].toUpperCase())) return msg.reply("please give a valid type")
-	let type = args.shift().toUpperCase()
-	await client.user.setActivity(`${args.join(" ")}`, { type: type })
-	msg.reply('done')
+    if (
+      !["PLAYING", "STREAMING", "LISTENING", "WATCHING", "COMPETING"].includes(
+        args[0].toUpperCase()
+      )
+    )
+      return msg.reply("please give a valid type");
+    let type = args.shift().toUpperCase();
+    await client.user.setActivity(`${args.join(" ")}`, { type: type });
+    msg.reply("done");
   },
 };
