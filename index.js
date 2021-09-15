9128148c4c56fd5ae5c764bb6c2552270383a9a4
OOF2510 <OOF2510@users.noreply.github.com>
2021-09-15 20:50:58 +0000

    [auto] Code formatting

--- index.js
+++ index.js
@@ -75,11 +75,13 @@ client.on("ready", () => {
     console.log(`🗸 Loaded ${cmd.name}`);
   });
   console.log(client);
-  if(fs.existsSync('./temp/lastStatus.json')) require("./util/setStatus")(client);
-  else client.user.setActivity(
-     `${client.guilds.cache.size} servers! | ${config.prefix}help`,
-     { type: "WATCHING" }
-   );
+  if (fs.existsSync("./temp/lastStatus.json"))
+    require("./util/setStatus")(client);
+  else
+    client.user.setActivity(
+      `${client.guilds.cache.size} servers! | ${config.prefix}help`,
+      { type: "WATCHING" }
+    );
   const auth = config.mysql;
   const options = {
     host: auth.ip,
--- util/setStatus.js
+++ util/setStatus.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
 let config = require("../config.json");
-let lastStatus = require('../temp/lastStatus.json')
+let lastStatus = require("../temp/lastStatus.json");
 
 module.exports = function (client) {
-  if (lastStatus) return client.user.setActivity(`${lastStatus.name}`, {type: lastStatus.type});
-  else return client.user.setActivity(`${client.guilds.cache.size} servers! | ${config.prefix}help`,{ type: "WATCHING" });
+  if (lastStatus)
+    return client.user.setActivity(`${lastStatus.name}`, {
+      type: lastStatus.type,
+    });
+  else
+    return client.user.setActivity(
+      `${client.guilds.cache.size} servers! | ${config.prefix}help`,
+      { type: "WATCHING" }
+    );
 };
