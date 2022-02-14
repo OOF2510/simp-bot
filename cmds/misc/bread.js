4988e9ef48639811fef446b038ad3698ed3a1f3e
Nate T <gamerkid01234@gmail.com>
2022-02-14 18:02:58 +0000

    add bread command

---
+++ cmds/misc/bread.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "bread",
+    aliases: ["givebread", "breadpls"],
+    cat: "misc",
+    usage: "bread",
+    desc: "gives bread to user",
+    async execute(
+      msg
+    ) {
+        msg.reply(`Here is your bread: :bread:`) // wow what a sellout cant even afford bread
+    },
+  };
+  
