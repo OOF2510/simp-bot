1d5e29da2f1e1d2dea89085a2c0aafdc6e246e2e
Nate T <gamerkid01234@gmail.com>
2022-03-17 17:58:28 +0000

    BREAD

--- cmds/misc/bread.js
+++ cmds/misc/bread.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("cmd_name")
-    .setDescription("cmd desc"),
+    .setName("bread")
+    .setDescription("gives bread to user"),
   async execute(interaction, client, config, db, Discord, allowed) {
     let msg = interaction
-
+    msg.reply(`Here is your bread: :bread:`); // wow what a sellout cant even afford bread
   },
 };
