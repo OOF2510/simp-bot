7f94ed282ba1017278d780f9e9646793b34f9199
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2022-09-10 15:55:13 -0400

    his name is Ye

--- cmds/fun/kanyequote.js
+++ cmds/fun/kanyequote.js
@@ -4,14 +4,14 @@ const kanye = require("kanye.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("kanyequote")
-    .setDescription("Gives a random quote by Kanye West"),
+    .setDescription("Gives a random quote by Ye (Kanye West)"),
   async execute(interaction, client, config, db, Discord, allowed) {
     let msg = interaction;
 
     let { quote } = await kanye();
     let quoteEm = new Discord.EmbedBuilder()
       .setTitle(`"${quote}"`)
-      .setFooter({ text: `- Kanye West` })
+      .setFooter({ text: `- Ye` })
       .setColor(config.embedColor);
     msg.reply({ embeds: [quoteEm] });
   },
