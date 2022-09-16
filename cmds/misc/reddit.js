87dd321eb824bca81545b3040a4cc4f953d5bdec
Nate T <64423517+OOF2510@users.noreply.github.com>
2022-09-16 12:43:15 -0400

    add reddit command

---
+++ cmds/misc/reddit.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("reddit")
+    .setDescription("get a random post from specified subreddit")
+    .addStringOption((option) =>
+      option
+        .setName("subreddit")
+        .setDescription("subreddit name (without 'r/')")
+        .setRequired(true)
+    )
+    ,
+  async execute(interaction, client, config, db, Discord, allowed) {
+    let msg = interaction
+    let sub = interaction.options.getString("subreddit");
+    
+    try {
+        let em = new Discord.embedBuilder()
+        let post = await getPost(sub);
+        switch (post.selftext) {
+          case "":
+            em.setTitle(`${post.title? post.title : 'Error getting title'}`).setImage(`${post.url}`).setColor(config.embedColor).setFooter({ text: `${post.subreddit_name_prefixed}`, iconURL: 'https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png' })
+            msg.reply({ embeds: [em] });
+            break;
+          default:
+            em.setTitle(`${post.title? post.title : 'Error getting title'}`).setDescription(`${post.selftext? post.selftext : 'Error getting post content'}`).setColor(config.embedColor).setFooter({ text: `${post.subreddit_name_prefixed}`, iconURL: 'https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png' })
+            msg.reply({ embeds: [em] });
+            break;
+        }
+      } catch (e) {
+        console.log("Are you sure that subreddit exists?");
+      }
+  },
+};
