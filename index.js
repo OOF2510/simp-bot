de0f7530f247465fbf7406dde124d2b5f8e2ac3a
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2022-09-16 19:08:04 -0400

    fix

--- index.js
+++ index.js
@@ -130,8 +130,7 @@ client.on("interactionCreate", async (interaction) => {
   const { commandName } = interaction;
   const command = client.commands.get(commandName);
   if (!command) return;
-  if (!interaction.inGuild())
-    return interaction.reply(`You must use Simp Bot in a server`);
+  if (!interaction.inGuild()) return;
 
   interaction.author = interaction.user;
   interaction.send = interaction.reply;
