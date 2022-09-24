af8f6e2b4a4d76eec50537de6f29842c08a29c01
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2022-09-24 18:38:47 -0400

    fix akinator

--- cmds/fun/akinator.js
+++ cmds/fun/akinator.js
@@ -1,12 +1,31 @@
 const { SlashCommandBuilder } = require("discord.js");
+const akinator = require("discord.js-akinator");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("akinator")
-    .setDescription("akinator"),
+    .setDescription("akinator")
+    .addStringOption((option) =>
+      option
+        .setName("game-type")
+        .setDescription("Game type")
+        .addChoices(
+          { name: "Animal", value: "animal" },
+          { name: "Character", value: "character" },
+          { name: "Object", value: "object" }
+        )
+        .setRequired(false)
+    ),
   async execute(interaction, client, config, db, Discord, allowed) {
     let msg = interaction;
+    let gameType = interaction.options.getString("game-type");
 
-    msg.reply("re-working command, this is a placeholder");
+    akinator(msg, {
+      language: "en",
+      childMode: false,
+      gameType: `${gameType ? gameType : "character"}`,
+      useButtons: true,
+      embedColor: config.embedColor,
+    });
   },
 };
