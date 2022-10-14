35696d5c2d900c579a8147eccf368eead1c589af
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2022-10-14 13:02:48 -0400

    change skeleton to add-image

--- cmds/img/addimg.js
+++ cmds/img/addimg.js
@@ -3,23 +3,22 @@ const { CommandInteraction, Client } = require("discord.js"),
   Sequelize = require("sequelize");
 const Canvas = require("@napi-rs/canvas");
 /**
- * Add spooky scary skeleton to image
+ * Add image to image
  * @param {string|Buffer} image
+ * @param {string} type
  * @returns {Promise<Buffer>}
  */
-async function skel(image) {
+async function skel(image, type) {
   if (!image) throw new Error("image was not provided!");
 
-  let spooky = await Canvas.loadImage(
-    "https://cdn.discordapp.com/attachments/1021763955099717664/1027923627842207864/skeleton.png"
-  );
+  let spooky = await Canvas.loadImage(type);
   let img = await Canvas.loadImage(image);
   const canvas = Canvas.createCanvas(img.width, img.height);
   const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(
     spooky,
-    canvas.width / 2,
+    (canvas.width / 2) - 50,
     canvas.height / 2,
     canvas.width,
     canvas.height
@@ -29,10 +28,34 @@ async function skel(image) {
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("skeleton")
-    .setDescription("[IMG] puts a spooky scary skeleton on a user's pfp")
+    .setName("add-image")
+    .setDescription("[IMG] puts the chosen image on a user's pfp")
     .addUserOption((option) =>
       option.setName("user").setDescription("who?").setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("type").setDescription("Image you want to add").addChoices(
+        {
+          name: "skeleton",
+          value:
+            "https://cdn.discordapp.com/attachments/1021763955099717664/1027923627842207864/skeleton.png",
+        },
+        {
+          name: "rover",
+          value:
+            "https://cdn.discordapp.com/attachments/1021763955099717664/1030522543121109104/rover.png",
+        },
+        {
+          name: "timmy",
+          value:
+            "https://cdn.discordapp.com/attachments/1021763955099717664/1030523158387773461/timmy.png",
+        },
+        {
+          name: "cow",
+          value:
+            "https://cdn.discordapp.com/attachments/1021763955099717664/1030523692725325945/pfp-transparent.png",
+        }
+      ).setRequired(true)
     ),
   /**
    * Executes the command
@@ -45,13 +68,14 @@ module.exports = {
   async execute(interaction, client, config, db, allowed) {
     let msg = interaction;
     let user = interaction.options.getUser("user");
+    let type = interaction.options.getString('type')
     await msg.deferReply();
 
     let av = user.displayAvatarURL({ size: 512 });
 
-    let image = await skel(av);
+    let image = await skel(av, type);
     let file = new AttachmentBuilder(image, {
-      name: `${user.tag}-skeleton.png`,
+      name: `${user.tag}-addimg.png`,
     });
 
     msg.editReply({ files: [file] });
