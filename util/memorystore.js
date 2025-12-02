ad591551abb7d23a7c272027d8daec1ca9e33fbb
oof2510 <64423517+OOF2510@users.noreply.github.com>
2025-12-02 00:49:18 -0500

    ?

--- util/memorystore.js
+++ util/memorystore.js
@@ -1,8 +1,8 @@
-const { AiMemoryStore } = require('@oof2510/llmjs');
-const config = require('../config.json');
+import { AiMemoryStore } from "@oof2510/llmjs";
+import config from "../config.json";
 
 export const aiMemory = new AiMemoryStore({
   uri: config.mongodb.url,
   dbName: config.mongodb.database,
-  collectionName: 'ai_memory'
+  collectionName: "ai_memory",
 });
